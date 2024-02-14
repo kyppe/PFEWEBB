db95283ea053629c06c930f1180429faf1986613
malekbenhamadi <malek.ben.hamadi@outlook.com>
2024-02-14 15:52:16 +0100

    oo

--- src/app/models/adresse.ts
+++ src/app/models/adresse.ts
@@ -1,13 +1,16 @@
 export class Adresse {
+  id:number;
   lat: number;
   long: number;
   city: string;
   address: string;
 
-  constructor(lat: number, long: number, city: string, address: string) {
+  constructor(lat: number, long: number, city: string, address: string,id:number) {
     this.lat = lat;
     this.long = long;
     this.city = city;
     this.address = address;
+    this.id=id;
+
   }
 }
